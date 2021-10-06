b0b0a906899140766ebd90121ab9d235bacb02d7
ArtemSadovenko <90398799+ArtemSadovenko@users.noreply.github.com>
2021-10-07 00:10:53 +0300

    Add files via upload

---
+++ lab_4.1/lab_4.1/lab_4.1.cpp
@@ -0,0 +1,47 @@
+﻿#include <cmath>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    int k;
+    double s = 0;
+    int n = 15;
+
+    cout << "k = "; cin >> k;
+
+    i = k;
+
+    while (i <= n) {
+        s += cos(i) / (1.0 + pow(sin(i), 2));
+        i++;
+    }
+    cout << s << endl;
+    s = 0;
+    i = k;
+
+    do {
+        s += cos(i) / (1.0 + pow(sin(i), 2));
+        i++;
+    } while (i <= n);
+    cout << s << endl;
+
+    s = 0;
+    for (i = k; i <= n; i++) {
+        s += cos(i) / (1.0 + pow(sin(i), 2));
+    }
+    cout << s << endl;
+    s = 0;
+
+    for (i = n; i >= k; i--) {
+        s += cos(i) / (1.0 + pow(sin(i), 2));
+    }
+    cout << s << endl;
+
+    return 0;
+}
+
+
